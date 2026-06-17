bd2f7fb05f6597035bf5bcd3340a9ac1d23a3153
kenbim008 <kenbim008@example.com>
2026-06-16 18:50:52 -0600

    feat(services): add services barrel export

---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+export { DocumentService } from './DocumentService'
+export { SigningService } from './SigningService'
+export {
+  ServiceError,
+  isServiceError,
+  serviceErrorToStatus,
+  type ServiceErrorCode,
+  type ServiceResult,
+} from './errors'
